05a092b084e43480a7db20f8e8eaa2a9c14fb513
Jose Miguel Mendez <jmmb.cl88@gmail.com>
2021-06-29 21:58:56 -0400

    Repositorio de Hackathon, Bootcamp Inacap-Coding Dojo, Alvaro Valenzuela-Jose Mendez

---
+++ static/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //Creamos el evento click del botón
+    $("#btn-actualizar").on("click",function(){
+      //Actualizamos la página
+        location.reload();
+    });
+});
+//$('.alert').ready(function() {
+  //funcion para que abra el modal de GameOver
+  //$('#popupModal2').modal('toggle')
+//});
